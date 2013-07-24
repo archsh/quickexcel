94ef818c28e8b41b9ec0f2b6773b5ca8307c0248
Mingcai SHEN <archsh@gmail.com>
2013-07-24 15:06:19 +0800

    updated.

--- source/quixcel/models/base.py
+++ source/quixcel/models/base.py
@@ -14,7 +14,7 @@ Base = declarative_base()
 
 class Customer(Base):
     __tablename__ = 'customers'
-    id         = Column(INTEGER, primary_key=True,doc=u'编号')
+    id         = Column(INTEGER, primary_key=True,doc=u'序号')
     name       = Column(VARCHAR(128),unique=True,nullable=False,doc=u'名称')
     short_name = Column(VARCHAR(50),unique=True,nullable=False,doc=u'简称')
     address    = Column(VARCHAR(128),nullable=False,doc=u'地址')
@@ -40,11 +40,11 @@ class Customer(Base):
     
 class Employee(Base):
     __tablename__ = 'employees'
-    id         = Column(INTEGER, primary_key=True)
-    name       = Column(VARCHAR(24),unique=True,nullable=False)
-    cellphone  = Column(VARCHAR(24),nullable=False)
-    email      = Column(VARCHAR(50))
-    comment    = Column(VARCHAR(256))
+    id         = Column(INTEGER, primary_key=True,doc=u'序号')
+    name       = Column(VARCHAR(24),unique=True,nullable=False,doc=u'姓名')
+    cellphone  = Column(VARCHAR(24),nullable=False,doc=u'手机')
+    email      = Column(VARCHAR(50),doc=u'邮件')
+    comment    = Column(VARCHAR(256),doc=u'备注')
 
     def __init__(self, name, email=None,cellphone=None, comment=None):
         self.name = name
@@ -57,13 +57,13 @@ class Employee(Base):
 
 class Product(Base):
     __tablename__ = 'products'
-    id         = Column(INTEGER, primary_key=True)
-    sn         = Column(VARCHAR(36),unique=True)
-    name_major = Column(VARCHAR(24),nullable=False)
-    name_minor = Column(VARCHAR(12),nullable=False)
-    name_ext   = Column(VARCHAR(12))
-    price      = Column(NUMERIC(8,2),nullable=False)
-    comment    = Column(VARCHAR(256))
+    id         = Column(INTEGER, primary_key=True,doc=u'序号')
+    sn         = Column(VARCHAR(36),unique=True,doc=u'编号')
+    name_major = Column(VARCHAR(24),nullable=False,doc=u'主型号')
+    name_minor = Column(VARCHAR(12),nullable=False,doc=u'辅型号')
+    name_ext   = Column(VARCHAR(12),doc=u'扩展型号')
+    price      = Column(NUMERIC(8,2),nullable=False,doc=u'价格')
+    comment    = Column(VARCHAR(256),doc=u'备注')
 
     def __init__(self, sn, name_major, name_minor=None, name_ext=None,
                  price=None, comment=None):
@@ -80,16 +80,16 @@ class Product(Base):
 
 class Delivery(Base):
     __tablename__ = 'deliveries'
-    id         = Column(INTEGER, primary_key=True)
-    sn         = Column(VARCHAR(24),unique=True,nullable=False)
-    customer_id= Column(ForeignKey(Customer.__tablename__+'.id'),nullable=False)
-    product_id = Column(ForeignKey(Product.__tablename__+'.id'),nullable=False)
-    num        = Column(INTEGER,default=0)
-    price      = Column(NUMERIC(8,2),default=0.0)
-    amount     = Column(NUMERIC(12,2),default=0.0)
-    employee_id= Column(ForeignKey(Employee.__tablename__+'.id'))
-    dlv_date   = Column(DATE,default=datetime.datetime.now().date())
-    comment    = Column(VARCHAR(256))
+    id         = Column(INTEGER, primary_key=True,doc=u'序号')
+    sn         = Column(VARCHAR(24),unique=True,nullable=False,doc=u'单号')
+    customer_id= Column(ForeignKey(Customer.__tablename__+'.id'),nullable=False,doc=u'客户')
+    product_id = Column(ForeignKey(Product.__tablename__+'.id'),nullable=False,doc=u'产品')
+    num        = Column(INTEGER,default=0,doc=u'数量')
+    price      = Column(NUMERIC(8,2),default=0.0,doc=u'价格')
+    amount     = Column(NUMERIC(12,2),default=0.0,doc=u'金额')
+    employee_id= Column(ForeignKey(Employee.__tablename__+'.id'),doc=u'业务员')
+    dlv_date   = Column(DATE,default=datetime.datetime.now().date(),doc=u'发货日期')
+    comment    = Column(VARCHAR(256),doc=u'备注')
     customer = relationship("Customer", backref=backref("deliveries", order_by=id))
     product  = relationship("Product", backref=backref("deliveries", order_by=id))
     employee = relationship("Employee", backref=backref("deliveries", order_by=id))
@@ -110,13 +110,13 @@ class Delivery(Base):
 
 class Receipt(Base):
     __tablename__ = 'receipts'
-    id         = Column(INTEGER, primary_key=True)
-    customer_id= Column(ForeignKey(Customer.__tablename__+'.id'),nullable=False)
-    product_id = Column(ForeignKey(Product.__tablename__+'.id'),nullable=False)
-    amount     = Column(NUMERIC(12,2),default=0.0)
-    account    = Column(VARCHAR(64),nullable=False)
-    recvdate   = Column(DATE,default=datetime.datetime.now().date())
-    comment    = Column(VARCHAR(256))
+    id         = Column(INTEGER, primary_key=True,doc=u'序号')
+    customer_id= Column(ForeignKey(Customer.__tablename__+'.id'),nullable=False,doc=u'客户')
+    product_id = Column(ForeignKey(Product.__tablename__+'.id'),nullable=False,doc=u'产品')
+    amount     = Column(NUMERIC(12,2),default=0.0,doc=u'金额')
+    account    = Column(VARCHAR(64),nullable=False,doc=u'帐号')
+    recvdate   = Column(DATE,default=datetime.datetime.now().date(),doc=u'收款日期')
+    comment    = Column(VARCHAR(256),doc=u'备注')
     customer = relationship("Customer", backref=backref("receiptions", order_by=id))
     product  = relationship("Product", backref=backref("receiptions", order_by=id))
 
